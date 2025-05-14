cffb6f2f7c4e697223a91084170930a2b857afae
KevinAlpizarDev <kevinalpizardev@gmail.com>
2025-05-14 09:00:08 -0600

    Home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index]
   def index
+    @courses = Course.all.limit(1)
+    @latest_courses = Course.all.limit(3).order(created_at: :desc)
+
+    
   end
 end
