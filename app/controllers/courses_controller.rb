ebaad5132da5192f01f78a88ea2e5eb7bb50a508
KevinAlpizarDev <kevinalpizardev@gmail.com>
2025-06-04 00:38:39 -0600

    Adding username to a register

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -65,6 +65,6 @@ class CoursesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def course_params
-      params.expect(course: [ :title, :description ])
+      params.expect(course: [ :title, :description, :username ])
     end
 end
