bbfa292e403281bc17759bcf8435b5d7432cf667
KevinAlpizarDev <kevinalpizardev@gmail.com>
2025-06-03 21:24:27 -0600

    Stimulus tu navbar and main

---
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -0,0 +1,45 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu"]
+
+  connect() {
+    this.contentTarget = document.querySelector('main')
+    this.setInitialState()
+
+    window.addEventListener('resize', () => this.setInitialState())
+  }
+
+  disconnect() {
+    window.removeEventListener('resize', () => this.setInitialState())
+  }
+
+  toggleMenu() {
+    this.menuTarget.classList.toggle("hidden")
+    this.updatePadding()
+  }
+
+  setInitialState() {
+    if (window.innerWidth >= 768) {
+      this.menuTarget.classList.remove("hidden")
+      this.contentTarget.classList.add("pt-16")
+      this.contentTarget.classList.remove("pt-24")
+    } else {
+      this.menuTarget.classList.add("hidden")
+      this.contentTarget.classList.add("pt-16")
+      this.contentTarget.classList.remove("pt-24")
+    }
+  }
+
+  updatePadding() {
+    if (window.innerWidth < 768) {
+      if (this.menuTarget.classList.contains("hidden")) {
+        this.contentTarget.classList.add("pt-16")
+        this.contentTarget.classList.remove("pt-24")
+      } else {
+        this.contentTarget.classList.add("pt-24")
+        this.contentTarget.classList.remove("pt-16")
+      }
+    }
+  }
+}
