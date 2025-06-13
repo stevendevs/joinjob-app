b66e71fafd33a30b5157ff7051bbf0bfb24d0b17
KevinAlpizarDev <kevinalpizardev@gmail.com>
2025-06-13 14:14:02 -0600

    Pundit added

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -21,7 +21,7 @@ end
   # GET /courses/new
   def new
     @course = Course.new
-
+    authorize @course
   end
 
   # GET /courses/1/edit
@@ -32,6 +32,7 @@ end
   # POST /courses or /courses.json
   def create
     @course = Course.new(course_params)
+    authorize @course
     @course.user = current_user
 sleep 2
     respond_to do |format|
@@ -47,6 +48,7 @@ sleep 2
 
   # PATCH/PUT /courses/1 or /courses/1.json
   def update
+    authorize @course
     respond_to do |format|
       if @course.update(course_params)
         format.html { redirect_to @course, notice: "Course was successfully updated." }
@@ -60,6 +62,7 @@ sleep 2
 
   # DELETE /courses/1 or /courses/1.json
   def destroy
+    authorize @course
     @course.destroy
 
     respond_to do |format|
--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -6,6 +6,28 @@ class CoursePolicy < ApplicationPolicy
   end
   
   def edit?
-    @user.has_role?:admin
+    @user.has_role?:admin || @record.user = @user
   end
+
+
+
+
+  def update?
+    @user.has_role?:admin || @record.user = @user
+  end
+
+  def new?
+    @user.has_role?:teacher
+  end
+
+  def create?
+    @user.has_role?:teacher
+  end
+
+  def destroy?
+    @user.has_role?:admin || @record.user = @user
+  end
+
+
+
 end
